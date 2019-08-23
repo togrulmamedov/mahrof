5f4503dc8d439a3f1ac95416ea41da76b0e07f1b
tohrul <tohrul@ukr.net>
2019-08-23 12:35:16 +0300

    Modified tag removal.

--- xml_handler.py
+++ xml_handler.py
@@ -1,6 +1,11 @@
 # module xml_handler.py
+# see https://docs.python.org/3.7/library/xml.etree.elementtree.html
 
 import xml.etree.ElementTree as ET
+
+def removeTag(tagNameStr, parent):
+    for tag in parent.findall(tagNameStr):
+        parent.remove(tag)
 
 tree = ET.parse('mahrofstore.xml')
 root = tree.getroot()
@@ -9,13 +14,8 @@ offers = shop[5]
 
 for offer in offers:
     # removing unused elements
-    for delivery in offer.findall('delivery'):
-        offer.remove(delivery)
-
-    for pickup in offer.findall('pickup'):
-        offer.remove(pickup)
-
-    for sales_notes in offer.findall('sales_notes'):
-        offer.remove(sales_notes)
+    removeTag('delivery', offer)
+    removeTag('pickup', offer)
+    removeTag('sales_notes', offer)
 
 tree.write('output.xml', encoding='UTF-8', xml_declaration=True)
