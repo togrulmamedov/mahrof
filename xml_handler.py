a6623152e1bef167e52b15ae91c7e2efe7313094
tohrul <tohrul@ukr.net>
2019-08-23 16:36:06 +0300

    Finished initial edit.

--- xml_handler.py
+++ xml_handler.py
@@ -1,21 +1,120 @@
 # module xml_handler.py
 # see https://docs.python.org/3.7/library/xml.etree.elementtree.html
+# see https://lxml.de/tutorial.html
 
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+
+from lxml import etree as ET
 
 def removeTag(tagNameStr, parent):
     for tag in parent.findall(tagNameStr):
         parent.remove(tag)
 
-tree = ET.parse('mahrofstore.xml')
+def grabLastPictureTagIndex(parentTag):
+    pictureTagsCount = len(parentTag.findall('picture'))
+
+    return (pictureTagsCount + 4)
+
+def reInsert(index, tag, parent):
+    parent.remove(tag)
+    parent.insert(index, tag)
+
+def createParam(name, text):
+    param = ET.Element('param')
+    param.set('name', name)
+    param.text = text
+
+    return param
+
+parser = ET.XMLParser(remove_blank_text=True)
+tree = ET.parse('mahrofstore.xml', parser)
 root = tree.getroot()
 shop = root[0]
 offers = shop[5]
 
 for offer in offers:
+    vendorNameText = offer.find('vendor').text if offer.find('vendor') is not None else 'Ашгабатская Текстильная фабрика'
+    vendorCodeText = offer.find('vendorCode').text
+    offerNameText = offer.find('name').text
+
     # removing unused elements
     removeTag('delivery', offer)
     removeTag('pickup', offer)
     removeTag('sales_notes', offer)
+    removeTag('country_of_origin', offer)
+    removeTag('vendorCode', offer)
 
-tree.write('output.xml', encoding='UTF-8', xml_declaration=True)
+    lastPictureTagIndex = grabLastPictureTagIndex(offer)
+
+    #removing unnecessary text from description
+    for description in offer.findall('description'):
+        sliceIndex = str(description.text).find('опт/розница')
+        description.text = '<![CDATA[' + str(description.text)[:sliceIndex].rstrip() + ']]>'
+        reInsert(lastPictureTagIndex + 2, description, offer)
+
+    for vendor in offer.findall('vendor'):
+        vendorName = vendor.text
+        reInsert(lastPictureTagIndex, vendor, offer)
+
+    stock_quantity = ET.Element('stock_quantity')
+    stock_quantity.text = '1'
+
+    offer.insert(lastPictureTagIndex + 1, stock_quantity)
+
+    dimensions = None
+    color = None
+    equipment = createParam('Комлпектация', 'Один предмет')
+    features = None
+    material = createParam('Материал', 'Махровые')
+    density = None
+    decor = None
+    purpose = None
+    manufacturerCountry = createParam('Страна-производитель товара', 'Туркменистан')
+    registrationCountry = createParam('Страна регистрации бренда', 'Туркменистан')
+
+    width = None
+    length = None
+
+    for param in offer.findall('param'):
+        paramName = param.get('name', default=None)
+
+        if paramName is None:
+            continue
+
+        if paramName == 'Ширина':
+            width = param.text
+        elif paramName == 'Длина':
+            length = param.text
+        elif paramName == 'Цвет':
+            color = createParam('Цвет', str(param.text).capitalize())
+        elif paramName == 'Плотность':
+            density = createParam('Плотность материала', str(round(float(param.text))))
+        elif paramName == 'Хлопок':
+            features = createParam('Особенности материала', '100% хлопок')
+        elif paramName == 'Назначение полотенца':
+            purpose = createParam('Назначение', str(param.text))
+        elif paramName == 'Тематика декора, рисунка':
+            decor = createParam('Декорирование', str(param.text).strip().capitalize())
+
+        offer.remove(param)
+
+    if width is not None:
+        dimensions = createParam('Размеры', str(round(float(width))) + 'х' + str(round(float(length))) + ' см')
+        offer.append(dimensions)
+
+    if color is not None: offer.append(color)
+
+    offer.append(equipment)
+
+    if features is not None: offer.append(features)
+
+    offer.append(material)
+
+    if density is not None: offer.append(density)
+    if decor is not None: offer.append(decor)
+    if purpose is not None: offer.append(purpose)
+
+    offer.append(manufacturerCountry)
+    offer.append(registrationCountry)
+
+tree.write('output.xml', encoding='UTF-8', xml_declaration=True, pretty_print=True)
