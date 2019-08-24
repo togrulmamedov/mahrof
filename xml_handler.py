cda7cf2d2cc3c3f38c1ab928523742201ba7d8f7
tohrul <tohrul@ukr.net>
2019-08-24 13:44:47 +0300

    Added new options.

--- xml_handler.py
+++ xml_handler.py
@@ -26,11 +26,31 @@ def createParam(name, text):
 
     return paramElement
 
+def insertVendorName(vendorName, text):
+    i = 0
+
+    for index, character in enumerate(text):
+        if character.isdigit():
+            i = index
+            break
+
+    if i == 0:
+        return None
+
+    return text[:i] + vendorName + ' ' + text[i:]
+
 parser = etree.XMLParser(remove_blank_text=True)
 tree = etree.parse('mahrofstore.xml', parser)
 root = tree.getroot()
 shop = root[0]
+currencies = shop[3]
 offers = shop[5]
+
+# removing unused currencies
+for currency in currencies:
+    idParam = currency.get('id', default=None)
+
+    if idParam != 'UAH': currencies.remove(currency)
 
 for offer in offers:
     vendorNameText = offer.find('vendor').text if offer.find('vendor') is not None else 'Ашгабатская Текстильная фабрика'
@@ -61,20 +81,30 @@ for offer in offers:
 
     offer.insert(lastPictureTagIndex + 1, stock_quantity)
 
+    for name in offer.findall('name'):
+        name.text = str(name.text).rstrip().replace("*", "х")
+        name.text += ' (' + vendorCodeText + ')'
+        textWithVendorName = insertVendorName(vendorNameText, name.text)
+
+        if textWithVendorName is not None:
+            name.text = insertVendorName(vendorNameText, name.text)
+
     dimensions = None
     color = None
     equipment = createParam('Комлпектация', 'Один предмет')
-    features = None
+    materialFeatures = None
     material = createParam('Материал', 'Махровые')
     density = None
     decor = None
     purpose = None
+    features = None
     manufacturerCountry = createParam('Страна-производитель товара', 'Туркменистан')
     registrationCountry = createParam('Страна регистрации бренда', 'Туркменистан')
 
     width = None
     length = None
 
+    # removing params
     for param in offer.findall('param'):
         paramName = param.get('name', default=None)
 
@@ -90,14 +120,17 @@ for offer in offers:
         elif paramName == 'Плотность':
             density = createParam('Плотность материала', str(round(float(param.text))))
         elif paramName == 'Хлопок':
-            features = createParam('Особенности материала', '100% хлопок')
+            materialFeatures = createParam('Особенности материала', '100% хлопок')
         elif paramName == 'Назначение полотенца':
             purpose = createParam('Назначение', str(param.text))
         elif paramName == 'Тематика декора, рисунка':
             decor = createParam('Декорирование', str(param.text).strip().capitalize())
+        elif paramName == 'Подарочная упаковка' and param.text == 'да':
+            features = createParam('Особенности', 'Подарочные')
 
         offer.remove(param)
 
+    # appending params
     if width is not None:
         dimensions = createParam('Размеры', str(round(float(width))) + 'х' + str(round(float(length))) + ' см')
         offer.append(dimensions)
@@ -106,15 +139,22 @@ for offer in offers:
 
     offer.append(equipment)
 
-    if features is not None: offer.append(features)
+    if materialFeatures is not None: offer.append(materialFeatures)
 
     offer.append(material)
 
     if density is not None: offer.append(density)
     if decor is not None: offer.append(decor)
     if purpose is not None: offer.append(purpose)
+    if features is not None: offer.append(features)
 
     offer.append(manufacturerCountry)
     offer.append(registrationCountry)
 
 tree.write('output.xml', encoding='UTF-8', xml_declaration=True, pretty_print=True)
+
+
+# простыня 113683
+# халат 113720
+# простыня и наволочка 113803
+# белье 113524
